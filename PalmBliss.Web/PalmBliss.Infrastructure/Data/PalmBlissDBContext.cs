a0f406a9cc0ff7fae42b8cdaf2a52906f785aede
Ramya.U <ramyayadav2016@gmail.com>
2023-09-20 17:41:30 +0530

    adding db context class and adding the db conn string

---
+++ PalmBliss.Web/PalmBliss.Infrastructure/Data/PalmBlissDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalmBliss.Infrastructure.Data
+{
+    public class PalmBlissDBContext : DbContext
+    {
+        public PalmBlissDBContext(DbContextOptions<PalmBlissDBContext> options) : base(options)
+        {
+                
+        }
+    }
+}
